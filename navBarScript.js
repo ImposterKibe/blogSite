7b60fee34193810aeed6ba3b0669a8abe1104277
ImposterKibe <kibestevemwa@gmail.com>
2017-02-11 14:59:35 +0300

    adding

---
+++ navBarScript.js
@@ -0,0 +1,15 @@
+
+function topDropDownMenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+function openSocialNav(){
+	document.getElementById("sociallinks").style.width="250px";
+}
+function closeSocialNav(){
+	document.getElementById("sociallinks").style.width="0";
+	}
