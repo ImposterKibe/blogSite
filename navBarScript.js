60106ee717a893ddd4b18072f481b1091e33f7d4
ImposterKibe <kibestevemwa@gmail.com>
2017-02-11 15:29:29 +0300

    final
    finalising social links navigation bar

--- navBarScript.js
+++ navBarScript.js
@@ -8,7 +8,10 @@ function topDropDownMenu() {
     }
 }
 function openSocialNav(){
+	document.getElementById("sociallinks").style.visibility="visible";
 	document.getElementById("sociallinks").style.width="250px";
+
+
 }
 function closeSocialNav(){
 	document.getElementById("sociallinks").style.width="0";
